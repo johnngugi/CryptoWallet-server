52098b6d90db6430883b2c43720e27723a8d40f2
John Ngugi <johnngugi777@gmail.com>
2019-12-17 00:47:41 +0300

    Stub authentication middleware

---
+++ application/test/fixtures/index.js
@@ -0,0 +1,9 @@
+const userProvider = require('./user');
+const currenciesProvider = require('./currency');
+const walletProvider = require('./wallet');
+
+module.exports = {
+    userProvider,
+    currenciesProvider,
+    walletProvider
+};

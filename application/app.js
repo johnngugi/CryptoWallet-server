480b4949ceef535136ad3f3abafe7dc5fb8d5211
John Ngugi <johnngugi777@gmail.com>
2019-12-17 20:03:26 +0300

    Install cors package Enable cors

--- application/app.js
+++ application/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,6 +9,7 @@ var apiRouter = require('./routes/api/index');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

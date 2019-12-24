f807e8b9a08ab603d11c71384b9d54cd7dbad51e
John Ngugi <johnngugi777@gmail.com>
2019-12-24 21:35:09 +0300

    Fetch all currencies

--- application/routes/api/index.js
+++ application/routes/api/index.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 var auth = require('./auth');
 var eth = require('./eth');
+var currency = require('./currency');
 
 router.use('/auth', auth);
 router.use('/eth', eth);
+router.use('/currency', currency);
 
 module.exports = router;
